c64fc027dd2811f5fab52d3248a22f7eab9a3a65
shinkuan <shinkuan318@gmail.com>
2024-01-21 23:21:18 +0800

    Remove mitm_playwright, playwright is now in mitm.py

--- mitm.py
+++ mitm.py
@@ -1,16 +1,21 @@
 import json
 import threading
 import asyncio
+import signal
 import time
+import re
 import mitmproxy.addonmanager
 import mitmproxy.http
 import mitmproxy.log
 import mitmproxy.tcp
 import mitmproxy.websocket
+from pathlib import Path
 from optparse import OptionParser
 from mitmproxy import proxy, options, ctx
 from mitmproxy.tools.dump import DumpMaster
 from xmlrpc.server import SimpleXMLRPCServer
+from playwright.sync_api import sync_playwright, WebSocket
+from playwright.sync_api._generated import Page
 
 activated_flows = [] # store all flow.id ([-1] is the recently opened)
 messages_dict = dict() # flow.id -> Queue[flow_msg]
@@ -39,6 +44,22 @@ class ClientWebSocket:
         activated_flows.remove(flow.id)
         messages_dict.pop(flow.id)
 
+class ClientHTTP:
+    def __init__(self):
+        pass
+
+    def request(self, flow: mitmproxy.http.HTTPFlow):
+        if flow.request.method == "GET":
+            if re.search(r'^https://game\.maj\-soul\.(com|net)/[0-9]+/v[0-9\.]+\.w/code\.js$', flow.request.url):
+                print("====== GET code.js ======"*3)
+                print("====== GET code.js ======"*3)
+                print("====== GET code.js ======"*3)
+                flow.request.url = "http://cdn.jsdelivr.net/gh/Avenshy/majsoul_mod_plus/safe_code.js"
+            elif re.search(r'^https://game\.mahjongsoul\.com/v[0-9\.]+\.w/code\.js$', flow.request.url):
+                flow.request.url = "http://cdn.jsdelivr.net/gh/Avenshy/majsoul_mod_plus/safe_code.js"
+            elif re.search(r'^https://mahjongsoul\.game\.yo-star\.com/v[0-9\.]+\.w/code\.js$', flow.request.url):
+                flow.request.url = "http://cdn.jsdelivr.net/gh/Avenshy/majsoul_mod_plus/safe_code.js"
+
 async def start_proxy(host, port, enable_unlocker, v10):
     opts = options.Options(listen_host=host, listen_port=port)
 
@@ -48,6 +69,7 @@ async def start_proxy(host, port, enable_unlocker, v10):
         # with_dumper=False,
     )
     master.addons.add(ClientWebSocket())
+    master.addons.add(ClientHTTP())
     if enable_unlocker:
         if v10:
             from unlocker_v10 import Unlocker
@@ -59,7 +81,7 @@ async def start_proxy(host, port, enable_unlocker, v10):
 
 # Create a XMLRPC server
 class LiqiServer:
-    _rpc_methods_ = ['get_activated_flows', 'get_messages', 'reset_message_idx']
+    _rpc_methods_ = ['get_activated_flows', 'get_messages', 'reset_message_idx', 'page_clicker']
     def __init__(self, host, port):
         self.host = host
         self.port = port
@@ -87,6 +109,11 @@ class LiqiServer:
         for flow_id in activated_flows:
             self.message_idx[flow_id] = 0
 
+    def page_clicker(self, xy):
+        global click_list
+        click_list.append(xy)
+        return True
+
     def serve_forever(self):
         print(f"XMLRPC Server is running on {self.host}:{self.port}")
         self.server.serve_forever()
@@ -98,6 +125,11 @@ if __name__ == '__main__':
         rpc_port = settings["Port"]["XMLRPC"]
         enable_unlocker = settings["Unlocker"]
         v10 = settings["v10"]
+        enable_playwright = settings["Playwright"]["enable"]
+        playwright_width = settings["Playwright"]["width"]
+        playwright_height = settings["Playwright"]["height"]
+        scale = playwright_width / 16
+
     mitm_host="127.0.0.1"
     rpc_host="127.0.0.1"
 
@@ -131,11 +163,43 @@ if __name__ == '__main__':
     server_thread = threading.Thread(target=lambda: liqiServer.serve_forever())
     server_thread.start()
 
+    page = None
+    if enable_playwright:
+        playwrightContextManager = sync_playwright()
+        playwright = playwrightContextManager.__enter__()
+        chromium = playwright.chromium
+        browser = chromium.launch_persistent_context(
+            user_data_dir=Path(__file__).parent / 'data',
+            headless=False,
+            viewport={'width': playwright_width, 'height': playwright_height},
+            proxy={"server": f"http://localhost:{mitm_port}"})
+
+        print(f'startup browser success')
+
+        page = browser.new_page()
+
+        page.goto('https://game.maj-soul.com/1/')
+        print(f'go to page success, url: {page.url}')
+
+    click_list = []
+    # On Ctrl+C, stop the other threads
     try:
         while True:
+            if enable_playwright:
+                if len(click_list) > 0:
+                    xy = click_list.pop(0)
+                    xy_scale = {"x":xy[0]*scale,"y":xy[1]*scale}
+                    print(f"page_clicker: {xy_scale}")
+                    page.mouse.move(x=xy_scale["x"], y=xy_scale["y"])
+                    time.sleep(0.1)
+                    page.mouse.click(x=xy_scale["x"], y=xy_scale["y"], delay=100)
+
+                    # page.locator("#layaCanvas").click(position=xy_scale)
             time.sleep(1)  # main thread will block here
     except KeyboardInterrupt:
         # On Ctrl+C, stop the other threads
+        if enable_playwright:
+            playwrightContextManager.__exit__()
         ctx.master.shutdown()
         liqiServer.server.shutdown()
         exit(0)
