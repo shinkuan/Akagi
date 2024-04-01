a709338ce10cf0efbd47cc77cd99282efab90697
shinkuan <shinkuan>
2024-04-01 22:16:51 +0800

    Remove unused import

--- akagi/__main__.py
+++ akagi/__main__.py
@@ -1,22 +1,20 @@
-import os
 import sys
-import platform
-import pathlib
-import subprocess
-import gevent as gvt
 from my_logger import both_logger
 from .common import start_message_controller, stop_message_controller, start_mitm, stop_mitm
-from .app import _app as app
+from .app import App
 from .config import config as akagi_config
 
+app = App()
 
 def stop():
+    global app
     stop_message_controller()
     stop_mitm()
     app.destroy()
     sys.exit(0)
 
 def main():
+    global app
     both_logger.info("Starting Akagi")
     app.protocol("WM_DELETE_WINDOW", stop)    
 
