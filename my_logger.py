0e68849867adb11de2095c4f0fabbf5ac0547310
shinkuan <shinkuan>
2024-05-02 11:47:24 +0800

    Catch error when getting logger.

--- my_logger.py
+++ my_logger.py
@@ -13,21 +13,25 @@ from aliyun.log.logger_hanlder import QueuedLogHandler, LogFields
 def my_sink(message: loguru.Message) -> None:
     record = message.record
 
-logger.level("CLICK", no=10, icon="CLICK")
-logger.add("akagi.log")
-# logger.add(my_sink)
-
-RECORD_LOG_FIELDS = set((LogFields.record_name, LogFields.level))
-res = requests.get("https://fastly.jsdelivr.net/gh/shinkuan/RandomStuff/aliyun_log_handler_arg.json", allow_redirects=True)
-json_data = json.loads(res.content)
-
-handler = QueuedLogHandler(
-    **json_data,
-    fields=RECORD_LOG_FIELDS,
-)
 game_result_logger = logging.getLogger("game_result_log")
 game_result_logger.setLevel(logging.INFO)
-game_result_logger.addHandler(handler)
+try:
+    logger.level("CLICK", no=10, icon="CLICK")
+    logger.add("akagi.log")
+    # logger.add(my_sink)
+
+    RECORD_LOG_FIELDS = set((LogFields.record_name, LogFields.level))
+    res = requests.get("https://cdn.jsdelivr.net/gh/shinkuan/RandomStuff/aliyun_log_handler_arg.json", allow_redirects=True)
+    json_data = json.loads(res.content)
+
+    handler = QueuedLogHandler(
+        **json_data,
+        fields=RECORD_LOG_FIELDS,
+    )
+
+    game_result_logger.addHandler(handler)
+except Exception as e:
+    logger.error(f"Failed to set up log handler: {e}")
 
 def game_result_log(mode_id: int, rank: int, score: int, model_hash: str) -> None:
     if any((mode_id is None, rank is None, score is None, model_hash is None)):
